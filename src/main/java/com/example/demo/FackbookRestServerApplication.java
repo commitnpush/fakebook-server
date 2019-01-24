d5e4fce3ea476ce68b137bad15c4d22e5f10b2d2
flynn <lognamu@gmail.com>
2019-01-24 19:25:15 +0900

    Initial commit from Spring Boot Starter

---
+++ src/main/java/com/example/demo/FackbookRestServerApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FackbookRestServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FackbookRestServerApplication.class, args);
+	}
+
+}
+
